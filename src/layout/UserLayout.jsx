1c6c1fda4693fc373722ee1b699e6c1f12967922
murad2005-057 <muradabbasli057.com>
2026-01-26 17:28:07 +0400

    footer

--- src/layout/UserLayout.jsx
+++ src/layout/UserLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 
 const UserLayout = () => {
@@ -8,6 +9,7 @@ const UserLayout = () => {
     <main>
       <Header />
         <Outlet />
+        <Footer/>
     </main>
   )
 }
