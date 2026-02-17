56866e971c99a2f9495ffe4aeaf8d31e644dfe75
projectnovruzzade <tunarwork99@gmail.com>
2026-02-18 00:30:50 +0400

    changed-api-file

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://travelio-production-f375.up.railway.app/";
 
 const request = async (endpoint, options = {}) => {
   const { method = "GET", body, headers = {} } = options;
