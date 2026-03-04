14e6498ea9c48e1b1d5ce66e589c150748b51d4f
projectnovruzzade <tunarwork99@gmail.com>
2026-03-04 22:55:00 +0400

    adding-env

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,4 @@
-// const BASE_URL = "https://travelio-production-0316.up.railway.app";
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const request = async (endpoint, options = {}) => {
   const { method = "GET", body, headers = {} } = options;
